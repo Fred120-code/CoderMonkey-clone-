90ef206a528c94f877b8d223b22ec81b37bf8158
Fred120-code <fredayemtsa@gmail.com>
2025-11-10 19:45:20 +0100

    first commit

---
+++ front-end/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Seo from "@/ui/components/seo";
+
+export default function Home() {
+	return(
+		<div>
+			<Seo title="title" description="hello world"/>
+			<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, tempore.</h1>
+		</div>
+	)
+}
