557cf1ba490d21e8330c5b995e6d3dd69561e2c8
Fred120-code <fredayemtsa@gmail.com>
2025-12-19 18:51:13 +0100

    implamentation de firebase store

---
+++ front-end/src/types/user.tsx
@@ -0,0 +1,19 @@
+import { Timestamp } from "firebase/firestore";
+
+export interface UserInterface{
+    uid:string,
+    email: string | null,
+    displayName: string | null,
+    emailVerified: boolean,
+    phoneNumber: string | null,
+    photoURL: string | null,
+    useDocument?: UserDocument
+}
+
+
+export interface UserDocument{
+    uid:string,
+    email:string,
+    how_did_hear:string,
+    creationDate: Timestamp
+}
